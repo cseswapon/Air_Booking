c051dedfa63fb73c414a40ff38483bdad4602736
cseswapon <cseswapon@gmail.com>
2023-03-13 00:44:11 +0600

    [ADD] - added with register 1st section

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
 import tbpLogo from "../../../assets/icon/logo/tbp_logo.png";
 const Register = () => {
   const stepper = [
@@ -19,9 +20,9 @@ const Register = () => {
   ];
   return (
     <div className="bg-slate-700 min-h-screen flex items-center justify-center">
-      <div className="flex items-center bg-gradient-to-r from-gray-800 to-gray-900 rounded">
+      <div className="flex items-center bg-gradient-to-r from-gray-800 to-gray-900 rounded relative">
         {/* login intro part */}
-        <div className="w-1/2 p-11 flex flex-col relative text-white">
+        <div className="w-1/2 p-11 flex flex-col text-white">
           <div className="my-3">
             <h1 className="my-2 font-bold text-3xl">
               {" "}
@@ -29,12 +30,14 @@ const Register = () => {
               Travel Business Portal
             </h1>
             <div className="my-5">
-              <button
-                type="button"
-                className="w-1/4 bg-blue-900 p-1 text-white rounded hover:text-black hover:bg-yellow-500 transition ease-in-out delay-150 hover:duration-300"
-              >
-                Login
-              </button>
+              <Link to={"/login"}>
+                <button
+                  type="button"
+                  className="w-1/4 bg-blue-900 p-1 text-white rounded hover:text-black hover:bg-yellow-500 transition ease-in-out delay-150 hover:duration-300"
+                >
+                  Login
+                </button>
+              </Link>
             </div>
             <p className="mt-6 mb-2 text-yellow-400 after_line">Reservation</p>
             <div className="flex items-center">
@@ -48,14 +51,14 @@ const Register = () => {
               <p className="ml-2">tbp@aotrek.com</p>
             </div>
           </div>
-          <div className="absolute w-1/4 bottom-0 right-0">
+          <div className="absolute w-32 bottom-0 left-0">
             <img src={tbpLogo} alt="avatar" />
           </div>
         </div>
         {/* login part */}
-        <div className="w-1/2 p-11 bg-slate-50">
-          <h1 className="text-center font-bold text-3xl my-3">Sing Up</h1>
-          <div className="flex justify-between text-center my-4">
+        <div className="w-2/2 p-11 bg-slate-50">
+          <h1 className="text-center font-bold text-3xl my-4">Sing Up</h1>
+          <div className="flex justify-between text-center my-5">
             {stepper.map((step, i) => (
               <div key={i}>
                 <button
@@ -79,7 +82,7 @@ const Register = () => {
                 </label>
                 <br />
                 <input
-                  className="w-12/12 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
+                  className="my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
                   type="text"
                   name="cname"
                   id="cname"
@@ -92,7 +95,7 @@ const Register = () => {
                 </label>
                 <br />
                 <input
-                  className="w-12/12 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
+                  className="my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
                   type="text"
                   name="pname"
                   id="pname"
@@ -100,77 +103,66 @@ const Register = () => {
                 />
               </div>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="grid grid-cols-2">
               <div className="mr-1">
-                <label htmlFor="division">
-                  Select Division <span className="text-red-500">*</span>
-                </label>
-                <br />
-                <select
-                  className="w-52 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
-                  name="division"
-                  id="division"
-                >
-                  <option value="bangladesh">Bangladesh</option>
-                  <option value="india">Bangladesh</option>
-                </select>
-              </div>
-              <div className="ml-1">
                 <label htmlFor="country">
                   Country <span className="text-red-500">*</span>
                 </label>
                 <br />
                 <select
-                  className="w-52 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
-                  name="country"
-                  id="country"
+                  className="w-full my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
+                  name=""
+                  id=""
                 >
-                  <option value="bangladesh">Bangladesh</option>
-                  <option value="india">Bangladesh</option>
+                  <option value="">Bangladesh</option>
+                </select>
+              </div>
+              <div className="ml-1">
+                <label htmlFor="country">
+                  Select Division <span className="text-red-500">*</span>
+                </label>
+                <br />
+                <select
+                  className="w-full my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
+                  name=""
+                  id=""
+                >
+                  <option value="">Bangladesh</option>
                 </select>
               </div>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="grid grid-cols-2">
               <div className="mr-1">
-                <label htmlFor="district">
+                <label htmlFor="country">
                   Select District <span className="text-red-500">*</span>
                 </label>
                 <br />
                 <select
-                  className="w-52 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
-                  name="district"
-                  id="district"
+                  className="w-full my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
+                  name=""
+                  id=""
                 >
-                  <option value="bangladesh">Bangladesh</option>
-                  <option value="india">Bangladesh</option>
+                  <option value="">Dhaka</option>
+                  <option value="">Tangail</option>
                 </select>
               </div>
-              <div className="mr-1">
-                <label htmlFor="division">
-                  Contact Number <span className="text-red-500">*</span>
+              <div className="ml-1">
+                <label htmlFor="country">
+                  Concat Number <span className="text-red-500">*</span>
                 </label>
                 <br />
-                <div className="flex items-center">
-                  <select
-                    className="w-20 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
-                    name="division"
-                    id="division"
-                  >
-                    <option disabled value="bangladesh">
-                      ---Select Number---
-                    </option>
-                    <option value="india">+88</option>
-                  </select>
-                  <input
-                    className="w-44 my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
-                    type="text"
-                  />
-                </div>
+                <input
+                  required
+                  className="my-1 p-2 bg-white drop-shadow-sm focus:outline-none border-0 rounded"
+                  type="text"
+                  name=""
+                  id=""
+                />
               </div>
             </div>
             <button
               type="submit"
-              className="w-1/4 bg-blue-900 p-1 text-white rounded hover:text-white hover:bg-pink-500 transition ease-in-out delay-150 hover:duration-300"
+              className="w-1/4 my-4 bg-blue-900 p-1 text-white rounded hover:text-white hover:bg-sky-500 transition ease-in-out delay-150 hover:duration-300"
             >
               Submit
             </button>
